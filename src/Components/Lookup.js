a21613dad53b805bae961cb1ce71d1493daddb23
Wes Basinger <wbasinger@villagetechschools.org>
2018-01-05 14:48:40 +0000

    Added the Cast Picker component to Lookup as well.

--- src/Components/Lookup.js
+++ src/Components/Lookup.js
@@ -1,11 +1,43 @@
 import React from 'react';
 
-const Lookup = () => {
-    return (
-        <div>
-            This is a lookup
-        </div>
-    )
+import CastPicker from './CastPicker'
+
+class Lookup extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            formComplete: false,
+            castId: "",
+            slug: ""
+        }
+        
+        this.handleCastChange = this.handleCastChange.bind(this);
+    }
+    
+    handleCastChange(fieldValue) {
+        const delim = fieldValue.split(":");
+        const castId =delim[0];
+        const slug = delim[1];
+        this.setState({castId, slug})
+    }
+    
+    render() {
+        
+        if (!this.state.formComplete) {
+            return(
+                <div>
+                    <form onSubmit={(e) => {
+                        e.preventDefault();
+                        // need to do a mutation with castID and slug here.
+                    }}>
+                        <CastPicker onCastChange={this.handleCastChange} />
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )
+        }
+    }
 }
 
 export default Lookup;
